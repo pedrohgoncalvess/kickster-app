2180cfc7a5c9c3089be652d7cfb2c02e7f690b1b
Pedro H <pedro_gonsalves@hotmail.com>
2023-09-28 22:55:08 -0300

    add: building nav bar

---
+++ src/components/AppNavbar.svelte
@@ -0,0 +1,24 @@
+<html>
+<nav class="main-nav">
+    <ul class="list-pages">
+        <li>Home</li>
+        <li>Analytics</li>
+        <li>Admin</li>
+    </ul>
+</nav>
+</html>
+
+<style>
+.list-pages {
+    display: flex;
+    gap: 25%;
+}
+
+.main-nav {
+    display: flex;
+    background-color: white;
+}
+.main-nav li {
+    /*margin: auto;*/
+}
+</style>
